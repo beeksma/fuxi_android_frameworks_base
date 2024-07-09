a8ca5edf73e555424f28d4c5d875ee5c1ed22eef
Yohei Yukawa <yukawa@google.com>
2024-07-09 07:31:26 +0000

    Mock out one more overload of Context#registerReceiver()
    This is a preparation before making
    
      InputMethodManagerService#mImeDrawsImeNavBarRes
    
    multi-user ready.
    
    Instantiating InputMethodManagerService results in invoking
    
      SystemLocaleWrapper#onStart(),
    
    which internally registers a BroadcastReceiver.  All the tests based
    on InputMethodManagerServiceTestBase are now failing to unregister
    such receivers.
    
    Let's make it no-op by mocking
    
      Context#registerReceiver(
          BroadcastReceiver, IntentFilter, String, Handler)
    
    for now.
    
    Bug: 345519864
    Test: atest FrameworksInputMethodSystemServerTests
    Flag: TEST_ONLY
    Change-Id: Ia457d631eda0f04a7acdd92315b896a97aa4fcb1

--- services/tests/InputMethodSystemServerTests/src/com/android/server/inputmethod/InputMethodManagerServiceTestBase.java
+++ services/tests/InputMethodSystemServerTests/src/com/android/server/inputmethod/InputMethodManagerServiceTestBase.java
@@ -35,7 +35,9 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import android.app.ActivityManagerInternal;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.IntentFilter;
 import android.content.pm.PackageManagerInternal;
 import android.content.res.Configuration;
 import android.hardware.input.IInputManager;
@@ -183,6 +185,9 @@ public class InputMethodManagerServiceTestBase {
         doNothing().when(mContext).enforceCallingPermission(anyString(), anyString());
         doNothing().when(mContext).sendBroadcastAsUser(any(), any());
         doReturn(null).when(mContext).registerReceiver(any(), any());
+        doReturn(null).when(mContext).registerReceiver(
+                any(BroadcastReceiver.class),
+                any(IntentFilter.class), anyString(), any(Handler.class));
         doReturn(null)
                 .when(mContext)
                 .registerReceiverAsUser(any(), any(), any(), anyString(), any(), anyInt());
