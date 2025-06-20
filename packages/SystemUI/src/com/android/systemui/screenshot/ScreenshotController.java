2ba0a1ccbe811dffce1249cd3bda9612b3184ec7
songlongdaiduong <song.long.dai.duong@gmail.com>
2025-06-20 13:20:05 +0800

    Revert "SystemUI: screenshot: Catch RejectedExecutionException"
    This reverts commit dd772a6aa8ae0073f53568539371fb7739a80cb9.

--- packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
@@ -110,7 +110,6 @@ import java.util.UUID;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.RejectedExecutionException;
 import java.util.function.Consumer;
 
 import javax.inject.Provider;
@@ -281,10 +280,7 @@ public class ScreenshotController implements ScreenshotHandler {
     private final TaskStackChangeListener mTaskListener = new TaskStackChangeListener() {
         @Override
         public void onTaskStackChanged() {
-            try {
-                mBgExecutor.execute(() -> updateForegroundTaskSync());
-            } catch (RejectedExecutionException e) {
-            }
+            mBgExecutor.execute(() -> updateForegroundTaskSync());
         }
     };
 
