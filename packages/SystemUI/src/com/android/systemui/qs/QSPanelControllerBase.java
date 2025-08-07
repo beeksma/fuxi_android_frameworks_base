adityayyy/POS_fwb

SystemUI: Introduce three-target ringer mode tile
Inspired by <> OS

Change-Id: I1c2262b915ed2fd9f1a02cf676ebad22de2912d6

packages/SystemUI/src/com/android/systemui/qs/QSPanelControllerBase.java
@@ -41,7 +41,9 @@ import com.android.systemui.qs.customize.QSCustomizerController;
 import com.android.systemui.qs.external.CustomTile;
 import com.android.systemui.qs.logging.QSLogger;
 import com.android.systemui.qs.tiles.FlashlightStrengthTile;
+import com.android.systemui.qs.tiles.RingerModeTile;
 import com.android.systemui.qs.tileimpl.QSTileViewImpl;
+import com.android.systemui.qs.tileimpl.RingerQSTileViewImpl;
 import com.android.systemui.qs.tileimpl.SliderQSTileViewImpl;
 import com.android.systemui.qs.tileimpl.SlideableQSTile;
 import com.android.systemui.scene.shared.flag.SceneContainerFlag;
@@ -384,6 +386,7 @@ public abstract class QSPanelControllerBase<T extends QSPanel> extends ViewContr
             tileView = new QSTileViewImpl(
                     getContext(), collapsedView, longPressEffect);
         }
+        final QSTileView createdtileView = createTileView(tile, collapsedView, longPressEffect);
         final TileRecord r = new TileRecord(tile, tileView);
         // TODO(b/250618218): Remove the QSLogger in QSTileViewImpl once we know the root cause of
         // b/250618218.
@@ -393,11 +396,30 @@ public abstract class QSPanelControllerBase<T extends QSPanel> extends ViewContr
                 qsTileView.setQsLogger(mQSLogger);
             }
         } catch (ClassCastException e) {
-            Log.e(TAG, "Failed to cast QSTileView to QSTileViewImpl", e);
+            // Log.e(TAG, "Failed to cast QSTileView to QSTileViewImpl", e);
         }
         mView.addTile(r);
         mRecords.add(r);
         mCachedSpecs = getTilesSpecs();
+    }
+
+    private QSTileView createTileView(final QSTile tile, boolean collapsedView,
+            QSLongPressEffect longPressEffect) {
+        switch (tile.getTileSpec()) {
+            case FlashlightStrengthTile.TILE_SPEC:
+                TouchableQSTile touchableTile = (TouchableQSTile) tile;
+                return new SliderQSTileViewImpl(
+                        getContext(),
+                        collapsedView,
+                        touchableTile.getTouchListener(),
+                        touchableTile.getSettingsSystemKey(),
+                        touchableTile.getSettingsDefaultValue());
+            case RingerModeTile.TILE_SPEC:
+                return new RingerQSTileViewImpl(getContext());
+            default:
+                return new QSTileViewImpl(
+                        getContext(), collapsedView, longPressEffect);
+        }
     }
 
     /** */
@@ -644,4 +666,8 @@ public abstract class QSPanelControllerBase<T extends QSPanel> extends ViewContr
         @Nullable
         public QSTile.Callback callback;
     }
+}
+
+public class MyCustomTile extends QSTileImpl<QSTile.State> implements TouchableQSTile {
+    // my implementation
 }

beeksma/fuxi_android_frameworks_base

packages/SystemUI/src/com/android/systemui/qs/QSPanelControllerBase.java
@@ -42,6 +42,8 @@ import com.android.systemui.qs.external.CustomTile;
 import com.android.systemui.qs.logging.QSLogger;
 import com.android.systemui.qs.tiles.FlashlightStrengthTile;
 import com.android.systemui.qs.tiles.VolumeControlTile;
+import com.android.systemui.qs.tiles.RingerModeTile;
+import com.android.systemui.qs.tileimpl.RingerQSTileViewImpl;
 import com.android.systemui.qs.tileimpl.QSTileViewImpl;
 import com.android.systemui.qs.tileimpl.SliderQSTileViewImpl;
 import com.android.systemui.qs.tileimpl.SlideableQSTile;
@@ -355,18 +357,7 @@ public abstract class QSPanelControllerBase<T extends QSPanel> extends ViewContr
         } else {
             longPressEffect = null;
         }
-        final QSTileViewImpl tileView;
-        if (FlashlightStrengthTile.TILE_SPEC.equals(tile.getTileSpec())
-            || VolumeControlTile.TILE_SPEC.equals(tile.getTileSpec())) {
-            SlideableQSTile slideableQSTile = (SlideableQSTile) tile;
-            tileView = new SliderQSTileViewImpl(
-                    getContext(),
-                    collapsedView,
-                    slideableQSTile);
-        } else {
-            tileView = new QSTileViewImpl(
-                    getContext(), collapsedView, longPressEffect);
-        }
+        final QSTileView tileView = createTileView(tile, collapsedView, longPressEffect);
         final TileRecord r = new TileRecord(tile, tileView);
         // TODO(b/250618218): Remove the QSLogger in QSTileViewImpl once we know the root cause of
         // b/250618218.
@@ -376,11 +367,29 @@ public abstract class QSPanelControllerBase<T extends QSPanel> extends ViewContr
                 qsTileView.setQsLogger(mQSLogger);
             }
         } catch (ClassCastException e) {
-            Log.e(TAG, "Failed to cast QSTileView to QSTileViewImpl", e);
+            // Log.e(TAG, "Failed to cast QSTileView to QSTileViewImpl", e);
         }
         mView.addTile(r);
         mRecords.add(r);
         mCachedSpecs = getTilesSpecs();
+    }
+
+    private QSTileView createTileView(final QSTile tile, boolean collapsedView,
+            QSLongPressEffect longPressEffect) {
+        switch (tile.getTileSpec()) {
+            case FlashlightStrengthTile.TILE_SPEC:
+            case VolumeControlTile.TILE_SPEC:
+                SlideableQSTile slideableQSTile = (SlideableQSTile) tile;
+                return new SliderQSTileViewImpl(
+                        getContext(),
+                        collapsedView,
+                        slideableQSTile);
+            case RingerModeTile.TILE_SPEC:
+                return new RingerQSTileViewImpl(getContext());
+            default:
+                return new QSTileViewImpl(
+                        getContext(), collapsedView, longPressEffect);
+        }
     }
 
     /** */